f196dde3c8e036ac7186792ce0741c2629af6438
Berke Emre Deveci <emrebkrdvc@gmail.com>
2026-06-28 10:54:40 +0300

    chore: initialize Spring Boot backend

---
+++ src/test/java/com/siteflowai/backend/SiteflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siteflowai.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
